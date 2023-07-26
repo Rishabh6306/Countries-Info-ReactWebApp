052b4bf4befd7c95bb4f29a97f00c6359290b6c5
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-07-26 10:40:16 +0530

    Country-Info Project Started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Box from './Components/Box';
+
+function App() {
+  return (
+    <div>
+      <Box />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Box.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Box() {
+  return (
+    <>
+      <div className="border-solid border-2 border-white h-72 w-80">
+          <img src="" alt="img" />
+          <h1>India</h1>
+          <p>Hindi</p>
+          <p>Indian</p>
+          <p>India2</p>
+      </div>
+    </>
+  )
+}
+
+export default Box
